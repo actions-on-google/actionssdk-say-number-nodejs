bb406b76ab118ae8c5dde20ad7a4637791282b54
Leon Nicholls <lnicholls@google.com>
2016-11-18 10:57:52 -0800

    Actions SDK sample
    Change-Id: I808bc9a220927b3fbfd3dd30f8c988a5665852d3

---
+++ README.md
@@ -0,0 +1,36 @@
+# Actions On Google Actions SDK Sample for Node.js
+==================================================
+
+This sample shows how to create an action for the Google Assistant using the Actions SDK.
+
+
+## Setup Instructions
+
+# Pre-requisites
+ 1. Actions SDK: [https://developers.google.com/actions/develop/sdk/getting-started#getting-started](https://developers.google.com/actions/develop/sdk/getting-started#getting-started)
+ 2. Google Cloud SDK: [https://developers.google.com/actions/develop/sdk/getting-started#deploy-your-action-web-service](https://developers.google.com/actions/develop/sdk/getting-started#deploy-your-action-web-service)
+
+See the developer guide and release notes at https://developers.google.com/actions/ for more details.
+
+# Steps:
+ 1. Host the Node.js project in this repo (We recommend Google App Engine flexible environment).
+ 2. Preview the action using the gactions CLI: ./gactions preview --action_package agent.json --invocation_name "hello action" --preview_mins 1234
+ 3. Use the gactions simulator to test the action (try "talk to hello action"): ./gactions simulate
+
+## Documentation
+* Actions on Google: https://developers.google.com/actions/
+
+## References and How to report bugs
+* If you find any issues, please open a bug here on GitHub
+
+How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md
+
+License
+See LICENSE.md
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/) and the [Actions On Google Developer Terms of Service](https://developers.google.com/actions/docs/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [http://g.co/actionsdev](http://g.co/actionsdev)
---
+++ app.js
@@ -0,0 +1,67 @@
+// Copyright 2016, Google, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START app]
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+
+let ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+const RAW_INTENT = 'raw.input';
+
+let app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json({type: 'application/json'}));
+
+app.post('/', function (request, response) {
+  console.log('handle post');
+  const assistant = new ActionsSdkAssistant({request: request, response: response});
+
+  function mainIntent (assistant) {
+    console.log('mainIntent');
+    let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
+          'I can read out an ordinal like ' +
+          '<say-as interpret-as="ordinal">123</say-as></speak>');
+    assistant.ask(inputPrompt, [{'intent': RAW_INTENT}]);
+  }
+
+  function rawInput (assistant) {
+    console.log('rawInput');
+    if (assistant.getRawInput() === 'bye') {
+      assistant.tell('Goodbye!');
+    } else {
+      let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, ' + assistant.getRawInput() +
+            '<break time="2" /> ' +
+            '<audio src="https://storage.googleapis.com/agent-compat-ssml-demo.appspot.com/sounds/airplane_1.wav">' +
+            'an audio file</audio></speak>');
+      assistant.ask(inputPrompt, [{'intent': RAW_INTENT}]);
+    }
+  }
+
+  let actionMap = new Map();
+  actionMap.set(assistant.StandardIntents.MAIN, mainIntent);
+  actionMap.set(RAW_INTENT, rawInput);
+
+  assistant.handleRequest(actionMap);
+});
+
+// Start the server
+let server = app.listen(app.get('port'), function () {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
