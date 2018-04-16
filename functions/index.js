ecf31a537483f6190f3603bab669fcc85e1496e0
Shuyang Chen <shuyangc@google.com>
2018-04-16 12:29:58 -0700

    Convert to use client library v2
    Bug: 78033098
    Change-Id: I4c8306badee52d7a7a3383679d60775c922e3f93

--- functions/index.js
+++ functions/index.js
@@ -14,42 +14,23 @@
 
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-
-const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 const functions = require('firebase-functions');
+const {actionssdk} = require('actions-on-google');
 
-const NO_INPUTS = [
-  'I didn\'t hear that.',
-  'If you\'re still there, say that again.',
-  'We can stop here. See you soon.'
-];
+const app = actionssdk({debug: true});
 
-exports.sayNumber = functions.https.onRequest((request, response) => {
-  const app = new ActionsSdkApp({request, response});
-
-  function mainIntent (app) {
-    console.log('mainIntent');
-    let inputPrompt = app.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
-      'I can read out an ordinal like ' +
-      '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>', NO_INPUTS);
-    app.ask(inputPrompt);
-  }
-
-  function rawInput (app) {
-    console.log('rawInput');
-    if (app.getRawInput() === 'bye') {
-      app.tell('Goodbye!');
-    } else {
-      let inputPrompt = app.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
-        app.getRawInput() + '</say-as></speak>', NO_INPUTS);
-      app.ask(inputPrompt);
-    }
-  }
-
-  let actionMap = new Map();
-  actionMap.set(app.StandardIntents.MAIN, mainIntent);
-  actionMap.set(app.StandardIntents.TEXT, rawInput);
-
-  app.handleRequest(actionMap);
+app.intent('actions.intent.MAIN', (conv) => {
+  conv.ask('<speak>Hi! <break time="1"/> ' +
+    'I can read out an ordinal like ' +
+    '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>');
 });
+
+app.intent('actions.intent.TEXT', (conv, input) => {
+  if (input === 'bye') {
+    return conv.close('Goodbye!');
+  }
+  conv.ask('<speak>You said, ' +
+    `<say-as interpret-as="ordinal">${input}</say-as></speak>`);
+});
+
+exports.sayNumber = functions.https.onRequest(app);
